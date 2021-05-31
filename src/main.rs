2f7123d2a2b6628bd01e1d2d90c674a1ba899e78
Johann Maree <jnmaree@protonmail.com>
2021-05-31 22:02:48 +0200

    cargo: .lock/.toml file addition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::string;
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let x = "Hello";
+    let y = "World";
+
+    let mut z = x.to_owned();
+    z.push_str(y);
+
+    println!("Z:{}", z);
 }
+
