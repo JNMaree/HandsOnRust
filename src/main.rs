1d11071c83c92c902e85cbd4fa5737a7b0a7b139
Johann Maree <jnmaree@protonmail.com>
2021-10-23 20:45:15 +0200

    passphrase string eq method added

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
 #![allow(non_snake_case)]
 #![warn(clippy::all, clippy::pedantic)]
 
+use std::io::stdin;
 //const STRING_BUFFER: usize = 256;
 
 fn main() {
     let list = ["one","two","three"];
-    for i in &list{
-        println!("list:{}", i);
+    let mut pass = false;
+    while !pass {
+        pass = read_passphrase(&list);
     }
+    println!("program completed successfully!");
+}
 
+fn read_passphrase(list: &[&str]) -> bool{
+    let mut uinput = String::new();
+    println!("passphrase:");
+    stdin().read_line(&mut uinput).expect("read_failed");
+    for i in list {
+        if i.eq(&uinput.trim()) {
+            println!("user accepted:{:?}", i);
+            uinput.clear();
+            return true
+        }
+    }
+    println!("phrase:{:?} not recognised!", uinput.trim());
+    uinput.clear();
+    false
 }
