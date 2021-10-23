4e3aaac927a7051e82b67b4baf9599380f9ce1f4
Johann Maree <jnmaree@protonmail.com>
2021-10-23 19:31:17 +0200

    Chapter1: Completion

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-### Hands-on Rust
-# by Herbert Wolverson
+# Hands-on Rust
+#### by Herbert Wolverson
 
 Effective learning through 2D game development and play.
+
+### Progress:
+
+- [x] **Chapter 1:** Rust and Your Development Environment
+- [] **Chapter 2:** First Steps with Rust
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
 #![allow(non_snake_case)]
-
-mod parser;
+#![warn(clippy::all, clippy::pedantic)]
 
 //const STRING_BUFFER: usize = 256;
 
 fn main() {
-    println!("start_program:");
-    
-    //Program Logic Start
-    let args: Vec<String> = std::env::args().collect();
-
-    match args.len() {
-        1 => usage(),
-        2 => parse_input_file(),
+    let list = ["one","two","three"];
+    for i in &list{
+        println!("list:{}", i);
     }
 
-    println!("end_program.");
 }
