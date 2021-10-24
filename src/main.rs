bfe40587014724c07e277d99127ed1a7d1652688
Johann Maree <jnmaree@protonmail.com>
2021-10-24 13:59:40 +0200

    chapter2: Fn, Struct, Vector addition

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@
 #![warn(clippy::all, clippy::pedantic)]
 
 use std::io::stdin;
-//const STRING_BUFFER: usize = 256;
 
 fn main() {
-    let list = ["one","two","three"];
+    let mut list: Vec<Phrase> = Vec::new();
+    list.push(Phrase::new("one",1));
+    list.push(Phrase::new("two",2));
+    list.push(Phrase::new("three",3));
     let mut pass = false;
     while !pass {
         pass = read_passphrase(&list);
@@ -13,13 +15,35 @@ fn main() {
     println!("program completed successfully!");
 }
 
-fn read_passphrase(list: &[&str]) -> bool{
+struct Phrase{
+    pass: String,
+    numeric: i8
+}
+
+impl Phrase{
+    fn new(str:&str, num:i8) -> Self {
+        Self{
+            pass:str.to_lowercase(),
+            numeric:num
+        }
+    }
+
+    fn print_pass(&self) {
+        println!("pass:{}, numeric:{}", self.pass, self.numeric);
+    }
+
+    fn eq(&self, phrase:&str) -> bool{
+        return self.pass.eq(phrase);
+    }
+}
+
+fn read_passphrase(list: &[Phrase]) -> bool{
     let mut uinput = String::new();
     println!("passphrase:");
     stdin().read_line(&mut uinput).expect("read_failed");
     for i in list {
         if i.eq(&uinput.trim()) {
-            println!("user accepted:{:?}", i);
+            i.print_pass();
             uinput.clear();
             return true
         }
